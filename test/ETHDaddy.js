7e4c94b4f66c3711c67c2e8cc42b27581385604f
royweb <xytank@gmail.com>
2022-12-24 13:36:40 -0500

    Add simple test

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,17 @@
 const { expect } = require("chai")
-
+ 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("ETHDaddy", () => {
+  
+  it('has a name', async () => {
+    // Deploy contract
+    const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
+    let ethDaddy = await ETHDaddy.deploy()
 
+    const result = await ethDaddy.name()
+    expect(result).to.equal('WRI Domain')
+  })
 })
