cc8b195236815e1630922db69b2f2571321743fa
royweb <xytank@gmail.com>
2022-12-27 14:39:24 -0500

    Import ERC721 contracts from openzeppelin

--- contracts/ETHDaddy.sol
+++ contracts/ETHDaddy.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract ETHDaddy {
-  string public name = "WRI Domain";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// 'is' to inherit from ERC721 contract
+contract ETHDaddy is ERC721 {
+
+  // 'constructor' only run the code once
+  constructor(string memory _name, string memory _symbol)
+    ERC721(_name, _symbol) 
+  { 
+    // Coode
+  }
+
 }
--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -5,13 +5,27 @@ const tokens = (n) => {
 }
 
 describe("ETHDaddy", () => {
+  let ethDaddy;
   
-  it('has a name', async () => {
+  const NAME = 'WRI Domain';
+  const SYMBOL = 'WRID';
+
+  beforeEach(async () => {
     // Deploy contract
     const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
-    let ethDaddy = await ETHDaddy.deploy()
-
-    const result = await ethDaddy.name()
-    expect(result).to.equal('WRI Domain')
+    ethDaddy = await ETHDaddy.deploy('WRI Domain', 'WRID')
   })
+
+  describe("Deployment", () => {
+    it('has a name', async () => {
+      const result = await ethDaddy.name()
+      expect(result).to.equal(NAME)
+    })
+  
+    it('has a symbol', async () => {
+      const result = await ethDaddy.symbol()
+      expect(result).to.equal(SYMBOL)
+    })
+  })
+
 })
