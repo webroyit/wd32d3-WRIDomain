9958e808202d05c823a510d3e81a869e9aa7918b
royweb <xytank@gmail.com>
2023-01-25 12:54:17 -0500

    Add Layout

--- src/App.js
+++ src/App.js
@@ -26,10 +26,22 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
+
+      <Search />
+
       <div className='cards__section'>
 
-        <h2 className='cards__title'>Welcome to ETH Daddy</h2>
+        <h2 className='cards__title'>Why you need a domain name.</h2>
+        <p className='cards__description'>
+          Own your custom username, use it across services, and
+          be able to store an avatar and other profile data.
+        </p>
 
+        <hr />
+
+        <div className='cards'>
+          
+        </div>
       </div>
 
     </div>
